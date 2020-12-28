5d5426f2cc12b148093110d676d9f9741f427ca4
Tom Kwong <tk3369@gmail.com>
2020-12-28 07:55:24 +0000

    Auto-create repo in ECR and better messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,42 @@
 # Build, tag, push, and deploy image.
 
 if [ $# -ne 2 ]; then
-    echo "Usage: `basename $0` [docker-repo] [tag]"
+    echo "Usage: `basename $0` [image] [tag]"
     exit 1
 fi
 
-REPO="$1"
+IMAGE="$1"
 TAG="$2"
-FUNC_NAME="${REPO}"
+FUNC_NAME="${IMAGE}"
 
-if [[ -z "${AWS_ACCOUNT_ID}" ]]; then
-    echo "Error: please set AWS_ACCOUNT_ID environment variable"
+if [[ -z "${IMAGE_REGISTRY}" ]]; then
+    echo "Error: please set IMAGE_REGISTRY environment variable e.g. <aws_account_id>.dkr.ecr.us-west-2.amazonaws.com"
     exit 1
 fi
 
-DKR="${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com"
-IMAGE_URI="${DKR}/${REPO}:${TAG}"
-DELAY=15
+IMAGE_URI="${IMAGE_REGISTRY}/${IMAGE}:${TAG}"
 
-docker build -t ${REPO} . \
-  && docker tag ${REPO}:${TAG} ${IMAGE_URI} \
-  && docker push ${IMAGE_URI} \
-  && aws lambda update-function-code --function-name ${FUNC_NAME} --image-uri ${IMAGE_URI} --publish
+echo "Building docker image"
+docker build -t ${IMAGE} . \
+  && docker tag ${IMAGE}:${TAG} ${IMAGE_URI}
 
-echo "It usually takes 15 seconds to take effect..."
-sleep $DELAY
+# Find out if the repository already exist
+# If not, create it first before pushing the image.
+aws ecr describe-repositories --output yaml 2>&1 |grep "repositoryName: ${FUNC_NAME}$" >/dev/null
+if [ $? -ne 0 ]         # does not exist?
+then
+    echo "Repository does not exist... creating one automatically."
+    aws ecr create-repository --repository-name "${FUNC_NAME}"
+fi
+
+# Push image now!
+docker push ${IMAGE_URI}
+
+# Find out if the function already exist.
+aws lambda get-function-configuration --function-name ${FUNC_NAME} --output yaml 2>&1|grep FunctionName
+if [ $? -eq 0 ]
+then
+    aws lambda update-function-code --function-name ${FUNC_NAME} --image-uri ${IMAGE_URI} --publish
+else
+    echo "Lambda function does not exist yet. Please create it first before deploying."
+fi
