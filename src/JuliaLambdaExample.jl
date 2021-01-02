c909df0cd0e8cff17983ba09cf767330e96d6bbc
Tom Kwong <tk3369@gmail.com>
2021-01-02 19:36:20 +0000

    Refactor code with clear/separate parse/result functions

--- src/JuliaLambdaExample.jl
+++ src/JuliaLambdaExample.jl
@@ -2,9 +2,6 @@ module JuliaLambdaExample
 
 using JSON
 
-# Must export `handle_event` function
-export handle_event
-
 # Put any extra initialization code here
 function __init__()
     @info "Initializing module"
@@ -18,19 +15,25 @@ function monte_carlo_pi(n)
     return hits / n * 4
 end
 
-# Handle a lambda invocation event
-function handle_event(event_data, headers = [])
-    @info "Handling request" event_data headers
-
-    parsed_data = JSON.parse(event_data)
-    simulations = get(parsed_data, "simulations", 0)
-    if simulations > 0
-        my_pi = monte_carlo_pi(simulations)
-        return JSON.json(Dict("pi" => my_pi))
+# Parse event data
+function get_simulations(event_data)
+    try
+        parsed_data = JSON.parse(event_data)
+        return parsed_data["simulations"]
+    catch ex
+        throw(ArgumentError("Please specify the number of simulations"))
     end
+end
 
-    # Raising an exception would cause the event to be logged and discarded
-    error("Please specify the number of simulations")
+# Construct return value
+make_result(value) = JSON.json(Dict("pi" => value)) 
+
+# Handle a lambda invocation event
+function handle_event(event_data, headers)
+    @info "Handling request" event_data headers
+    simulations = get_simulations(event_data)
+    my_pi = monte_carlo_pi(simulations)
+    return make_result(my_pi)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,6 @@ using JSON
 
 @testset "JuliaLambdaExample.jl" begin
     event_data = JSON.json(Dict("simulations" => 100_000))
-    result = handle_event(event_data)
-    @test_nowarn JSON.parse(result)
+    result = JuliaLambdaExample.handle_event(event_data, [])
     @test haskey(JSON.parse(result), "pi")
 end
