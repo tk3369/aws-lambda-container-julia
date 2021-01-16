2146e9d532b3e7cecc613ab95afae47630216644
Tom Kwong <tk3369@gmail.com>
2021-01-16 23:52:12 +0000

    Minimize metaprogramming

--- main.jl
+++ main.jl
@@ -1,13 +1,10 @@
-const mod = Ref{Symbol}()
-const func = Ref{Symbol}()
-
 const AWS_LAMBDA_RUNTIME_API = ENV["AWS_LAMBDA_RUNTIME_API"]
 const LAMBDA_TASK_ROOT = ENV["LAMBDA_TASK_ROOT"]
-const HANDLER = ENV["_HANDLER"]
+const HANDLER = ENV["_HANDLER"]  # from Dockerfile's CMD command
 const RUNTIME_URL = "http://$AWS_LAMBDA_RUNTIME_API/2018-06-01"
 
 # Using curl as HTTP client.
-# (Don't want to introduce HTTP dependency for faster initialization)
+# (Don't want to introduce HTTP.jl dependency for faster initialization)
 
 function http_get(url)
     cmd = `curl --silent --show-error --location --include --request GET $url`
@@ -24,8 +21,7 @@ function http_post(url, headers, body)
     return read(cmd, String)
 end
 
-# Convenient functions
-
+# Convenient function to post errors to the lamdba execution environment
 function post_error(path::AbstractString, ex::Exception)
     headers = [
         "Content-type" => "application/json",
@@ -41,62 +37,57 @@ function post_error(path::AbstractString, ex::Exception)
     end
 end
 
-# Init function to load user module and locate function
-function init()
-    try
-        @info "Loading user module"
-        mod[], func[] = Symbol.(split(HANDLER, "."))
+# Initialize lambda function by loading user module
+try
+    @info "Loading user module"
+    global mod, func = Symbol.(split(HANDLER, "."))
 
-        @eval using $(mod[])
+    @eval using $(mod)
 
-        @info "Completed loading user module" mod[] func[]
-    catch ex
-        @error "Initializtion error" ex
-        post_error("runtime/init/error", ex)
-        @info "Shutting down container"
-        exit(1)
-    end
+    @info "Completed loading user module" mod func
+catch ex
+    @error "Initializtion error" ex
+    post_error("runtime/init/error", ex)
+    @info "Shutting down container"
+    exit(1)
 end
 
+@info "Resolve handler function"
+const my_module = Base.eval(Main, mod)
+const my_handler = Base.eval(my_module, func)
+
 # An infinite loop to process events
-function process_events()
-    @info "Start processing requests"
-    while true
-        local state, request_id
-        try
-            state = :start
-            request_headers, request_body = http_get("$RUNTIME_URL/runtime/invocation/next")
-            request_id_idx = findfirst(x -> x[1] == "Lambda-Runtime-Aws-Request-Id", request_headers)
-            request_id = request_headers[request_id_idx][2]
-            @info "Received event" request_id
-            state = :received
+@info "Start processing requests"
+while true
+    local state, request_id
+    try
+        state = :start
+        request_headers, request_body = http_get("$RUNTIME_URL/runtime/invocation/next")
+        request_id_idx = findfirst(x -> x[1] == "Lambda-Runtime-Aws-Request-Id", request_headers)
+        request_id = request_headers[request_id_idx][2]
+        @info "Received event" request_id
+        state = :received
 
-            response = @eval $(mod[]).$(func[])($request_body, $request_headers)
-            @info "Got response from handler" response
-            state = :handled
+        # response = @eval $(mod[]).$(func[])($request_body, $request_headers)
+        response  = my_handler(request_body, request_headers)
+        @info "Got response from handler" response
+        state = :handled
 
-            http_post("$RUNTIME_URL/runtime/invocation/$request_id/response", [], response)
-            @info "notified lambda runtime"
-            state = :finished
-        catch ex
-            if state == :start
-                @error "Unable to receive request" ex
-            elseif state == :received
-                @error "Unable to handle event" ex request_id
-                post_error("runtime/invocation/$request_id/error", ex)
-            elseif state == :handled
-                @error "Unable to notify lambda runtime (invocation response)" ex request_id
-            else
-                @error "Unknown state" ex state
-            end
+        http_post("$RUNTIME_URL/runtime/invocation/$request_id/response", [], response)
+        @info "notified lambda runtime"
+        state = :finished
+    catch ex
+        if state == :start
+            @error "Unable to receive request" ex
+        elseif state == :received
+            @error "Unable to handle event" ex request_id
+            post_error("runtime/invocation/$request_id/error", ex)
+        elseif state == :handled
+            @error "Unable to notify lambda runtime (invocation response)" ex request_id
+        else
+            @error "Unknown state" ex state
         end
     end
 end
 
-function main()
-    init()
-    process_events()
-    @info "Shutdown complete"
-end
-
-main()
+@info "All done"
